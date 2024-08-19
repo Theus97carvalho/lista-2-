5a113c9be7e969727f2b11b7d7a656e0ca663832
Theus97carvalho <167145188+Theus97carvalho@users.noreply.github.com>
2024-08-19 16:05:21 -0300

    Add files via upload

---
+++ 10 escola semestre aprovado reprovado - logica/10 escola semestre aprovado reprovado - logica/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_escola_semestre_aprovado_reprovado___logica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double P1;
+            double P2;
+            double media;
+
+            Console.WriteLine("Digite o valor da P1");
+            P1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da P2");
+            P2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (P1 + 2 * P2) / 3;
+            Console.WriteLine($"Média do aluno: {media:F2}");
+
+            if (media >= 5.0)
+            {
+                Console.WriteLine("Situação: Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Situação: Reprovado");
+
+
+            }
+        }
+    }
+}
---
+++ 11 nota necessaria - logica/11 nota necessaria - logica/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_nota_necessaria___logica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double P1;
+            double valorP2;
+
+            Console.WriteLine("Digite a nota da primeira avaliação (P1):");
+            P1 = double.Parse(Console.ReadLine());
+
+            valorP2 = (5 * 3 - P1) / 2;
+
+            Console.WriteLine($"Para ser aprovado, você precisa tirar no mínimo {valorP2:F2} na segunda avaliação (P2).");
+        }
+    }
+}
